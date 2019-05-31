7d3d8672c507a974217f9cd2007c93d3b0db83ce
BlayFlorian <blayflorian@gmail.com>
2019-05-31 11:33:54 +0200

    color

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -15,9 +15,21 @@ export class HomeComponent implements OnInit {
   public start: boolean;
   public user: User;
   public usersNeedHelp: any[];
+
+  // Timer
+  private time = 300;
+  private array = Array.from({length: 300 - 1}, (x, i) => i + 1);
+  public config: any = {
+    leftTime: this.time,
+    notify: this.array
+  };
+  public color = 'green';
+
   @ViewChild(CountdownComponent) counter: CountdownComponent;
 
-  constructor(private socket: Socket, private auth: AuthService, private service: ServiceService) { }
+
+  constructor(private socket: Socket, private auth: AuthService, private service: ServiceService) {
+  }
 
   ngOnInit() {
     this.auth.checkAuthentication().then((user: User) => {
@@ -49,6 +61,7 @@ export class HomeComponent implements OnInit {
       this.counter.restart();
     }).catch(next => console.log(next));
   }
+
   stopSession() {
     this.service.closeSession().then(res => {
       console.log(res);
@@ -56,5 +69,19 @@ export class HomeComponent implements OnInit {
     }).catch(err => {
       console.log(err);
     });
+  }
+
+  notify(ev) {
+    const percent = ev.left / (this.time * 10);
+    this.color = this.hsl_col_perc(percent);
+  }
+
+  hsl_col_perc(percent) {
+    const red = 0;
+    const green = 120;
+    const a = percent / 100;
+    const b = (green - red) * a;
+    const c = b + red;
+    return 'hsl(' + c + ', 100%, 50%)';
   }
 }
