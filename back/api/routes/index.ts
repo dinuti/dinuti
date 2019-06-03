42248bf6cc0dda1ae5cf38904ac2c79e7edd35f4
Brault <alex.brault35@gmail.com>
2019-06-03 11:25:36 +0200

    Back alert log

--- back/api/routes/index.ts
+++ back/api/routes/index.ts
@@ -4,11 +4,13 @@ import { UserRoutes } from './user-routes';
 import { ProfilesRoutes } from './profiles-routes';
 import { LocationRoutes } from './location-routes';
 import { SessionRoutes } from './session-routes';
+import{AlertRoutes} from './alert-route';
 const router: Router = Router();
 
 router.use('/user', UserRoutes);
 router.use('/profiles', ProfilesRoutes);
 router.use('/location', LocationRoutes);
 router.use('/session', SessionRoutes);
+router.use('/alert', AlertRoutes);
 
 export const MainRouter: Router = router;
